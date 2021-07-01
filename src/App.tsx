71567c834f83569c76f613f3205f4558813dea01
Vlad Prodan <vladprodan6@gmail.com>
2021-07-01 14:02:15 +0300

    create form, make request

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,23 @@ import * as counterSelect from './redux/selectors'
 import {useAppSelector} from "./app/hooks";
 import {Switch, Route} from 'react-router-dom'
 import {registerUser} from "./api/authApi";
-import {Register} from "./pages/Auth/Register/Register";
+import {Register} from "./pages/Register/Register";
 import { Layout } from './components/Layout';
 import { Header } from './components/Header/Header';
-import { Login } from './pages/Auth/Login/Login';
+import { Login } from './pages/Login/Login';
 import './App.css'
 
 function App() {
   const counter = useAppSelector(counterSelect.all)
   console.log(counter)
 
-  React.useEffect(() => {
-    const register = async () => {
-      const res = await registerUser('body');
-      console.log(res)
-    }
-    register().then(r => console.log(r))
-  },[])
-
-
   return (
-      <>
-          {/*<Header/>*/}
+      <Layout>
           <Switch>
               <Route component={Login} exact path='/'/>
-              <Route component={Register} exact path='/register'/>
+              <Route component={Register} path='/register'/>
           </Switch>
-      </>
+      </Layout>
   );
 }
 
--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -6,4 +6,4 @@ import BaseAxios from 'axios';
 axios.defaults.headers.common['Authorization'] = process.env.REACT_APP_API_KEY;
 // const axiosAuth = BaseAxios.create({baseURL: BASE_URL});
 
-export const registerUser = async (body: any) => await axios.post('/api/1.0/auth/login', body);
+export const registerUser = async (body: any) => await axios.post('/api/1.1/auth/login', body);
